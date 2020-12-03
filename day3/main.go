4c08e077596c6decab4339aae4140b9689322013
Brad Davies <me@varbrad.dev>
2020-12-03 19:08:15 +0000

    Day 3 - Part 2

--- day3/main.go
+++ day3/main.go
@@ -17,28 +17,56 @@ func main() {
 
 	fmt.Printf("Part 1 = %v", Day3Part1(input))
 	fmt.Println("")
+	fmt.Printf("Part 2 = %v", Day3Part2(input))
+	fmt.Println("")
 }
 
 // Day3Part1 solver
 func Day3Part1(input []string) int {
-	x, y := 3, 1
+	treemap := treeMap{
+		grid: input,
+		dx:   3,
+		dy:   1,
+	}
+	return treemap.solve()
+}
+
+// Day3Part2 solver
+func Day3Part2(input []string) int {
+	pairs := [][2]int{{1, 1}, {3, 1}, {5, 1}, {7, 1}, {1, 2}}
+	mul := 1
+	for _, dxdy := range pairs {
+		treemap := treeMap{input, dxdy[0], dxdy[1]}
+		mul *= treemap.solve()
+	}
+	return mul
+}
+
+type treeMap struct {
+	grid []string
+	dx   int
+	dy   int
+}
+
+func (treemap treeMap) solve() int {
+	x, y := treemap.dx, treemap.dy
 	trees := 0
-	height := len(input)
-	width := len(input[0])
+	height := len(treemap.grid)
+	width := len(treemap.grid[0])
 
 	for {
 		if y >= height {
 			break
 		}
 
-		isTree := string(input[y][x]) == "#"
+		isTree := string(treemap.grid[y][x]) == "#"
 
 		if isTree {
 			trees++
 		}
 
-		x = (x + 3) % width
-		y++
+		x = (x + treemap.dx) % width
+		y += treemap.dy
 	}
 
 	return trees
--- day3/main_test.go
+++ day3/main_test.go
@@ -26,3 +26,26 @@ func TestDay3Part1(t *testing.T) {
 		t.Fatalf("The provided example case failed, expected %v, got %v", expected, result)
 	}
 }
+
+func TestDay3Part2(t *testing.T) {
+	input := []string{
+		"..##.......",
+		"#...#...#..",
+		".#....#..#.",
+		"..#.#...#.#",
+		".#...##..#.",
+		"..#.##.....",
+		".#.#.#....#",
+		".#........#",
+		"#.##...#...",
+		"#...##....#",
+		".#..#...#.#",
+	}
+
+	result := Day3Part2(input)
+	expected := 336
+
+	if result != expected {
+		t.Fatalf("The provided example case failed, expected %v, got %v", expected, result)
+	}
+}
