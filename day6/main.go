679a4361bd2849ac8e7d3a0566bb7a0707a50216
Brad Davies <me@varbrad.dev>
2020-12-06 18:43:59 +0000

    Day 6 - Part 2

--- day6/main.go
+++ day6/main.go
@@ -18,6 +18,7 @@ func main() {
 	input := string(contents)
 
 	utils.Part1(Day6Part1(input))
+	utils.Part2(Day6Part2(input))
 }
 
 // Day6Part1 solver
@@ -36,10 +37,45 @@ func Day6Part1(input string) int {
 	return yesAnswers
 }
 
+// Day6Part2 solver
+func Day6Part2(input string) int {
+	lineRegex := regexp.MustCompile(`\n`)
+	whitespaceRegex := regexp.MustCompile(`\s+`)
+	trimmedInput := strings.TrimSpace(input)
+	groups := strings.Split(trimmedInput, "\n\n")
+
+	yesAnswers := 0
+
+	for _, s := range groups {
+		people := len(lineRegex.FindAllStringIndex(s, -1)) + 1
+		answers := whitespaceRegex.ReplaceAllString(s, "")
+		yesAnswers += getAllYes(answers, people)
+	}
+
+	return yesAnswers
+}
+
 func getUniqueChars(input string) int {
 	m := make(map[string]bool)
 	for _, s := range input {
 		m[string(s)] = true
 	}
 	return len(m)
+}
+
+func getAllYes(input string, count int) int {
+	m := make(map[string]int)
+	for _, s := range input {
+		m[string(s)]++
+	}
+
+	maxes := 0
+
+	for _, v := range m {
+		if v == count {
+			maxes++
+		}
+	}
+
+	return maxes
 }
--- day6/main_test.go
+++ day6/main_test.go
@@ -30,3 +30,28 @@ func TestDay6Part1(t *testing.T) {
 
 	assert.Equal(t, expected, actual)
 }
+
+func TestDay6Part2(t *testing.T) {
+	input := `
+		abc
+
+		a
+		b
+		c
+
+		ab
+		ac
+
+		a
+		a
+		a
+		a
+
+		b
+	`
+
+	expected := 6
+	actual := Day6Part2(input)
+
+	assert.Equal(t, expected, actual)
+}
