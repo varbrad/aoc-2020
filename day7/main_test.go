f14485ffd113cf0c775d67873e564d61e12dd9ab
Brad Davies <me@varbrad.dev>
2020-12-24 13:07:41 +0000

    Day 7 - Part 2 unit test

--- day7/main_test.go
+++ day7/main_test.go
@@ -24,3 +24,20 @@ func TestDay7Part1(t *testing.T) {
 
 	assert.Equal(t, expected, actual)
 }
+
+func TestDay7Part2(t *testing.T) {
+	input := []string{
+		"shiny gold bags contain 2 dark red bags.",
+		"dark red bags contain 2 dark orange bags.",
+		"dark orange bags contain 2 dark yellow bags.",
+		"dark yellow bags contain 2 dark green bags.",
+		"dark green bags contain 2 dark blue bags.",
+		"dark blue bags contain 2 dark violet bags.",
+		"dark violet bags contain no other bags.",
+	}
+
+	expected := 126
+	actual := Day7Part2(input)
+
+	assert.Equal(t, expected, actual)
+}
