a36c94ace7320be55ff33c8b6a40260db1e52c53
Brad Davies <me@varbrad.dev>
2020-12-24 17:58:53 +0000

    Day 9 - Part 2

--- day9/main.go
+++ day9/main.go
@@ -14,6 +14,7 @@ func main() {
 	}
 
 	utils.Part1(Day9Part1(input, 25, 25))
+	utils.Part2(Day9Part2(input, 25, 25))
 }
 
 // Day9Part1 solver
@@ -35,6 +36,14 @@ func Day9Part1(numbers numberList, preamble int, length int) int {
 	}
 
 	return -1
+}
+
+// Day9Part2 solver
+func Day9Part2(numbers numberList, preamble int, length int) int {
+	target := Day9Part1(numbers, preamble, length)
+	set := numbers.contiguousSet(target)
+	min, max := set.minMax()
+	return min + max
 }
 
 type numberList []int
@@ -59,4 +68,40 @@ func (nl *numberList) contains(n int, excludedIndex int) bool {
 		}
 	}
 	return false
+}
+
+func (nl *numberList) sum() int {
+	sum := 0
+	for _, v := range *nl {
+		sum += v
+	}
+	return sum
+}
+
+func (nl *numberList) contiguousSet(n int) numberList {
+	length := len(*nl)
+	for i := 0; i < length-1; i++ {
+		for j := i + 1; j < length; j++ {
+			list := (*nl)[i : j+1]
+			sum := list.sum()
+			if sum == n {
+				return list
+			} else if sum > n {
+				break
+			}
+		}
+	}
+	return nil
+}
+
+func (nl *numberList) minMax() (int, int) {
+	min, max := (*nl)[0], (*nl)[0]
+	for _, v := range *nl {
+		if v < min {
+			min = v
+		} else if v > max {
+			max = v
+		}
+	}
+	return min, max
 }
--- day9/main_test.go
+++ day9/main_test.go
@@ -35,3 +35,33 @@ func TestDay9Part1(t *testing.T) {
 
 	assert.Equal(t, expected, actual)
 }
+
+func TestDay9Part2(t *testing.T) {
+	numbers := []int{
+		35,
+		20,
+		15,
+		25,
+		47,
+		40,
+		62,
+		55,
+		65,
+		95,
+		102,
+		117,
+		150,
+		182,
+		127,
+		219,
+		299,
+		277,
+		309,
+		576,
+	}
+
+	expected := 62
+	actual := Day9Part2(numbers, 5, 5)
+
+	assert.Equal(t, expected, actual)
+}
