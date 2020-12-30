3c767f91564cfb92f64fd6a62724e3dfe46cc1c5
Brad Davies <me@varbrad.dev>
2020-12-30 18:25:01 +0000

    Improve day 12 part 2 test coverage

--- day12/main_test.go
+++ day12/main_test.go
@@ -71,4 +71,37 @@ func TestDay12Part2(t *testing.T) {
 
 		assert.Equal(t, expected, actual)
 	})
+
+	t.Run(`Improve test coverage`, func(t *testing.T) {
+		input := []string{
+			"F10",
+			"S5",
+			"E22",
+			"W13",
+			"L90",
+			"L270",
+			"L90",
+			"R270",
+			"R180",
+			"L180",
+			"F3",
+			"F17",
+			"R90",
+			"F11",
+			"S20",
+			"R90",
+			"F5",
+			"R90",
+			"F5",
+			"R90",
+			"F5",
+			"R90",
+			"F5",
+		}
+
+		expected := 535
+		actual := Day12Part2(input)
+
+		assert.Equal(t, expected, actual)
+	})
 }
